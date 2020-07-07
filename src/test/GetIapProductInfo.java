dcbd6b981915ac29397e39736aa6b9d51065237c
Huy Bui <abcd@email.com>
2020-07-07 14:26:25 +0300

    Remove unnecessary classes

--- src/test/GetIapProductInfo.java
+++ src/test/GetIapProductInfo.java
@@ -1,26 +1,8 @@
 package test;
 
 import org.apache.http.client.methods.HttpGet;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.util.List;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.http.Consts;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import test.model.FileInfo;
-import test.model.FileServerOriResult;
 import test.model.ProductDetailQueryResp;
 
 public class GetIapProductInfo {
